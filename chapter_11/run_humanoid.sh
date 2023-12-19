019e407417abbb282168d15038d77a53a2fe29f1
ApressGryffinWinkler <104927373+ApressGryffinWinkler@users.noreply.github.com>
2023-12-19 16:17:21 -0500

    Delete chapter_11 directory

--- chapter_11/run_humanoid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -u -e  # Check for uninitialized variables and exit if any command fails.
-
-for (( seed=1; seed<=5; seed++ ))
-do
-    python3 ppo_continuous.py --environment_name=Humanoid-v4 --num_iterations=80 --seed=$seed --results_csv_path=logs/ppo/humanoid/$seed/results.csv
-done
--- chapter_11/run_pong.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -u -e  # Check for uninitialized variables and exit if any command fails.
-
-for (( seed=1; seed<=3; seed++ ))
-do
-    python3 ppo_atari.py --environment_name=Pong --num_iterations=30 --seed=$seed --results_csv_path=logs/ppo/pong/$seed/results.csv
-done
